835dee83a6f6443862c5785e20824ef95d2dcf0c
Victor T <littlevixplanet@icloud.com>
2024-03-27 11:43:24 -0700

    finished hide
    almost finished complex behaviours

--- FiniteMachine/Assets/Bot.cs
+++ FiniteMachine/Assets/Bot.cs
@@ -74,6 +74,31 @@ public class Bot : MonoBehaviour
             }
         }
         Seek(chosenSpot);
+    }void CleverHide()
+    {
+        float dist = Mathf.Infinity;
+        Vector3 chosenSpot = Vector3.zero;
+        Vector3 chosenDir = Vector3.zero;
+        GameObject chosenGO = World.Instance.GetHidingSpots()[0];
+        for(int i = 0; i < World.Instance.GetHidingSpots().Length; i++)
+        {
+            Vector3 hideDir = World.Instance.GetHidingSpots()[i].transform.position - target.transform.position;
+            Vector3 hidePos = World.Instance.GetHidingSpots()[i].transform.position + hideDir.normalized * 10;
+            
+            if(Vector3.Distance(this.transform.position, hidePos) < dist)
+            {
+                chosenSpot = hidePos;
+                chosenDir = hideDir;
+                chosenGO = World.Instance.GetHidingSpots()[i];
+                dist = Vector3.Distance(this.transform.position, hidePos);
+            }
+        }
+        Collider hideCol = chosenGO.GetComponent<Collider>();
+        Ray backRay = new Ray(chosenSpot, -chosenDir.normalized);
+        RaycastHit info;
+        float distance = 100.0f;
+        hideCol.Raycast(backRay, out info, distance);
+        Seek(info.point + chosenDir.normalized * 5);
     }
     void Wander()
     {
@@ -92,10 +117,39 @@ public class Bot : MonoBehaviour
 
         Seek(targetWorld);
     }
+    bool canSeeTarget()
+    {
+        RaycastHit raycastInfo;
+        Vector3 rayToTarget = target.transform.position - this.transform.position;
+        if (Physics.Raycast(this.transform.position, rayToTarget, out raycastInfo))
+        {
+            if (raycastInfo.transform.gameObject.tag == "cop")
+            {
+                return true;
+            }
+        }
+        return false;
 
+    }
     // Update is called once per frame
+    bool targetCanSeeMe()
+    {
+        Vector3 toAgent = this.transform.position - target.transform.position;
+        float lookingAngle = Vector3.Angle(target.transform.forward, toAgent);
+        if (lookingAngle < 60)
+            return true;
+        else 
+            return false;
+    }
+    bool cooldown = false;
     void Update()
     {
-        Hide();
+        if (canSeeTarget() && targetCanSeeMe())
+        {
+            CleverHide();
+        }
+        else
+            Pursue();
+        
     }
 }
